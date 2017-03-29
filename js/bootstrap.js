7acf3c6fb7c32f2fd813581167d903864a642b3b
lucimarNeves <lucimar.neves@gmail.com>
2017-03-29 12:31:35 -0300

    #Front All4U

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2375,3 +2375,27 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+jQuery(document).ready(function($) {
+
+      $('#myCarousel').carousel({
+              interval: 5000
+      });
+
+      //Handles the carousel thumbnails
+      $('[id^=carousel-selector-]').click(function () {
+      var id_selector = $(this).attr("id");
+      try {
+          var id = /-(\d+)$/.exec(id_selector)[1];
+          console.log(id_selector, id);
+          jQuery('#myCarousel').carousel(parseInt(id));
+      } catch (e) {
+          console.log('Regex failed!', e);
+      }
+  });
+      // When the carousel slides, auto update the text
+      $('#myCarousel').on('slid.bs.carousel', function (e) {
+               var id = $('.item.active').data('slide-number');
+              $('#carousel-text').html($('#slide-content-'+id).html());
+      });
+})(jQuery);
